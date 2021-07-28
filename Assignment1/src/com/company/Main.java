9ad9bfa9b383ef11f0d47ee29da7a9df23318b57
Aniket979Kanodia <kanodiaaniket979@gmail.com>
2021-07-29 00:50:36 +0530

    Handled File I/O exception and FileNotFound Exception and worked on output format

--- Assignment1/src/com/company/Main.java
+++ Assignment1/src/com/company/Main.java
@@ -1,8 +1,6 @@
 package com.company;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
 import java.util.HashMap;
@@ -11,6 +9,8 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) throws IOException{
+        try{
+            //Read text file containing ULRs
         File urlFile = new File("..\\Assignment1\\URLs.txt");
         String[] urlStr = new String[(int)urlFile.length()];
         Scanner scan = new Scanner(urlFile);
@@ -22,6 +22,7 @@ public class Main {
             urlsCount++;
         }
 
+        // Read text files containing words
         File wordsFile = new File("..\\Assignment1\\Words.txt");
         String[] wordStr = new String[(int) wordsFile.length()];
         Scanner sc = new Scanner(wordsFile);
@@ -31,13 +32,19 @@ public class Main {
             wordStr[j] = sc.next();
             j++;
         }
+        //ArrayList to store words to be serched
         ArrayList<String> fileContent = new ArrayList<String>(Arrays.asList(wordStr));
+        //HashMap to store words and their total  occurane for Output#2
         HashMap<String,Integer> finalCount = new HashMap<>();
+        System.out.println("Output #1");
+        System.out.println("=========");
+
+            //Loop for each url
         for(int l=0;l<urlsCount;l++) {
+            //Hashmap to store content of each url
             HashMap<String, Integer> eachUrlCount = new HashMap<>();
             ArrayList<Integer> urlWordsCount = new ArrayList<>();
-            int count = l + 1;
-            System.out.println("ULR " + count + " : " + urlStr[l]);
+            System.out.println(urlStr[l]);
             var url = new URL(urlStr[l]);
 
 
@@ -77,21 +84,27 @@ public class Main {
                     }
                     eachUrlCount.remove(s);
                 }
+            }catch (MalformedURLException e){
+                System.out.println("Exception : " + e);
             }
 
         }
         Collection<Integer> values = finalCount.values();
         ArrayList<Integer> listOfValues = new ArrayList<>(values);
         Collections.sort(listOfValues);
-        System.out.println();
-        System.out.println("#Output 2");
-        System.out.println();
+        System.out.println("==================================");
+        System.out.println("Output #2");
+        System.out.println("=========");
         int l= listOfValues.size()-1;
         for(int y=l;y>=0;y--){
             String s = getKey(finalCount, listOfValues.get(y));
             System.out.println(s+ " : "+ listOfValues.get(y));
             fileContent.remove(s);
         }
+        }catch(IOException e){
+            System.out.println("Exception:" + e);
+        }
+
     }
     public static <K, V> K getKey(Map<K, V> map, V value)
     {
