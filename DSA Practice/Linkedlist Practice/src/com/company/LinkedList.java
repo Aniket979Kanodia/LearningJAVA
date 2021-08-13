885732ae8242aa25ae192c6a7b2588d902734f51
Aniket979Kanodia <kanodiaaniket979@gmail.com>
2021-08-14 00:02:53 +0530

    Added method to insert element in the beginning and specific node

--- DSA Practice/Linkedlist Practice/src/com/company/LinkedList.java
+++ DSA Practice/Linkedlist Practice/src/com/company/LinkedList.java
@@ -4,7 +4,7 @@ public class LinkedList {
 
     Node head;
 
-    static class Node{
+    class Node{
         int data;
         Node next;
         Node(int d){
@@ -13,25 +13,40 @@ public class LinkedList {
         }
     }
 
-    public static LinkedList insert(LinkedList list,int data){
+    public void insert(int data){
         Node new_node = new Node(data);
         new_node.next = null;
 
-        if(list.head==null){
-            list.head = new_node;
+        if(head==null){
+            head = new_node;
         }
         else{
-            Node last= list.head;
+            Node last= head;
             while(last.next!=null){
                 last= last.next;
             }
             last.next= new_node;
         }
-        return list;
+        return;
+    }
+    public void push(int data){
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
     }
 
-    public static void printList(LinkedList list){
-        Node CurrNode = list.head;
+    public void insertAfter(Node prevNode, int data){
+        if(prevNode==null){
+            System.out.println("previous node cannot be null");
+            return;
+        }
+        Node newNode = new Node(data);
+        newNode.next=prevNode.next;
+        prevNode.next=newNode;
+    }
+
+    public void printList(){
+        Node CurrNode = head;
 
         System.out.println("LinkedList: ");
         while(CurrNode != null){
@@ -42,16 +57,19 @@ public class LinkedList {
 
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-        list = insert(list,1);
-        list = insert(list,2);
-        list = insert(list,3);
-        list = insert(list,4);
-        list = insert(list,5);
-        list = insert(list,6);
-        list = insert(list,7);
-        list = insert(list,8);
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        list.insert(5);
+        list.insert(6);
+        list.insert(7);
+        list.insert(8);
+        list.insert(9);
+        list.insert(10);
+        list.insertAfter(list.head.next,111 );
 
-        printList(list);
+        list.printList();
     }
 
 }
