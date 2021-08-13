6e99bbe6cb4fffc4ed0ba2ed6b5c858fb9b57e5f
Aniket979Kanodia <kanodiaaniket979@gmail.com>
2021-08-13 23:12:52 +0530

    added method to insert element at the end of list and to print the list

--- DSA Practice/Linkedlist Practice/src/com/company/LinkedList.java
+++ DSA Practice/Linkedlist Practice/src/com/company/LinkedList.java
@@ -4,13 +4,54 @@ public class LinkedList {
 
     Node head;
 
-    class Node{
+    static class Node{
         int data;
         Node next;
         Node(int d){
             data =d;
             next = null;
         }
+    }
+
+    public static LinkedList insert(LinkedList list,int data){
+        Node new_node = new Node(data);
+        new_node.next = null;
+
+        if(list.head==null){
+            list.head = new_node;
+        }
+        else{
+            Node last= list.head;
+            while(last.next!=null){
+                last= last.next;
+            }
+            last.next= new_node;
+        }
+        return list;
+    }
+
+    public static void printList(LinkedList list){
+        Node CurrNode = list.head;
+
+        System.out.println("LinkedList: ");
+        while(CurrNode != null){
+            System.out.print(CurrNode.data + " ");
+            CurrNode =CurrNode.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list = insert(list,1);
+        list = insert(list,2);
+        list = insert(list,3);
+        list = insert(list,4);
+        list = insert(list,5);
+        list = insert(list,6);
+        list = insert(list,7);
+        list = insert(list,8);
+
+        printList(list);
     }
 
 }
